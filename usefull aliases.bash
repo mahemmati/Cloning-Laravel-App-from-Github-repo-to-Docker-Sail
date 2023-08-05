9a3389bd57fb2171e249c4b468f66eb33dd477ba
mahemmati <128333525+mahemmati@users.noreply.github.com>
2023-08-05 07:23:02 +0330

    Updates usefull aliases.bash

--- usefull aliases.bash
+++ usefull aliases.bash
@@ -1,19 +1,24 @@
-alias s="bash vendor/bin/sail"
 alias sail="bash vendor/bin/sail"
+alias s="bash vendor/bin/sail"
+
+alias up="bash vendor/bin/sail up -d"
 alias u="bash vendor/bin/sail up -d"
+
+alias down="bash vendor/bin/sail down"
 alias d="bash vendor/bin/sail down"
 
 alias artisan="bash vendor/bin/sail artisan"
 alias a="bash vendor/bin/sail artisan"
 
-alias mmd="bash vendor/bin/sail artisan make:model"
-alias mmd-a="bash vendor/bin/sail artisan make:model --all"
+alias mkmd="bash vendor/bin/sail artisan make:model"
+alias mkmd-a="bash vendor/bin/sail artisan make:model --all"
 
-alias mcp="bash vendor/bin/sail artisan make:component"
-alias mcp-v="bash vendor/bin/sail artisan make:component --view"
+alias mkcp="bash vendor/bin/sail artisan make:component"
+alias mkcpvw="bash vendor/bin/sail artisan make:component --view"
 
 alias mg="bash vendor/bin/sail artisan migrate"
-alias mgf="bash vendor/bin/sail artisan migrate:fresh"
-alias mgf-s="bash vendor/bin/sail artisan migrate:fresh --seed"
+alias mgfr="bash vendor/bin/sail artisan migrate:fresh"
+alias mgfrsd="bash vendor/bin/sail artisan migrate:fresh --seed"
 
-alias t="bash vendor/bin/sail artisan test"
+alias test="bash vendor/bin/sail artisan test"
+alias ts="bash vendor/bin/sail artisan test"
